515685ddfd58d98032e80227e59c5f8fa1a61eeb
yuxiang wang <yuxiangwang@iheartmedia.com>
2015-08-25 05:52:03 -0400

    use chromeDriver

--- src/test/java/com/iheart/selenium/webAutomation/RunDailyTest.java
+++ src/test/java/com/iheart/selenium/webAutomation/RunDailyTest.java
@@ -14,7 +14,7 @@ import org.junit.rules.TestName;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RunDailyTest {
 
@@ -30,8 +30,8 @@ public class RunDailyTest {
 	
 	
 	
-	String browser = "firefox";
-//	String browser = "chrome";
+//	String browser = "firefox";
+	String browser = "chrome";
 	
 	static String userCity = "";
 	
